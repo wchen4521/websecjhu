a2d2a0d41787fe8255d5fc76a8bf3f76d004858b
Chen <wchen4521@gmail.com>
2024-09-29 15:10:52 -0700

    adding files for WildRydes

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-1_D1tustMru', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '37o8mt6ft02hv8tqe2o5l7o7j2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
