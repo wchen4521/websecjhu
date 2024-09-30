6bc00966badd29f4af27feee1f266379fcef87f3
Chen <wchen4521@gmail.com>
2024-09-29 19:27:23 -0700

    updated config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_D3VphIhDZ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'ec1vrlmu5v0l7iac4p1vcuilq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_rq3iKMnyQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3sjfld2c0uet5cgls8njllqsab', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
